e1295ca8456f5fd18468c5e3a4c568826a33df55
Oozls <oozls125@gmail.com>
2026-01-26 04:18:55 +0900

    hell nah i forgot to turn off debug again

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from waitress import serve
 from pages import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
-    # serve(app, host='0.0.0.0', port=8000)
+    # app.run(host='0.0.0.0', port=8000, debug=True)
+    serve(app, host='0.0.0.0', port=8000)
