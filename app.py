bff6159a841c6dfae3c697ba6f9b20f00771d02e
Oozls <oozls125@gmail.com>
2026-02-04 12:04:31 +0900

    fix: Turned off debug mode

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from waitress import serve
 from pages import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
-    # serve(app, host='0.0.0.0', port=8000)
+    # app.run(host='0.0.0.0', port=8000, debug=True)
+    serve(app, host='0.0.0.0', port=8000)
